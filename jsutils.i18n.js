9ee1e068a5d66f46814f289ada2d0e8a5433f00a
Mukul Jindal <jindal.k.mukul@gmail.com>
2016-04-21 12:49:23 +0530

    returning key if not available in map

--- jsutils.i18n.js
+++ jsutils.i18n.js
@@ -1,42 +1,56 @@
 define({
-  name: "jsutils.i18n",
-  modules: ["jQuery","jsutils.file","jsutils.tmpl"]
-}).as(function(i18n, jQuery, fileUtil,tempUtil) {
-    var STRINGS = {};
-    return {
-        _instance_ : function () {
-          this.reversemap = {};
-        },
-        add : function(file){
-          return fileUtil.getJSON(file).done(function(resp){
-            for(var key in resp.map){
-              STRINGS[key] = resp.map[key];
-            }
-          })
-        },
-        get : function (key) {
-          var keys = key.split(":");
-          var str = STRINGS[keys[0]] || "";
-          for(var i=1; i<keys.length; i++){
-            str = str.replace("$"+i,keys[i]);
-          }
-          if(this.reversemap){
-            this.reversemap[str] = key;
-          }
-          return str;
-        },
-        key : function(str){
-            if(this.reversemap){
-              return this.reversemap[str];
-            }
-        },
-        _ready_ : function(){
-          var i18n = this;
-          if(tempUtil){
-            tempUtil.formatter("i18n", function(key){
-              return i18n.get(key);
-            })
-          }
-        }
-    };
+	name : "jsutils.i18n",
+	modules : ["jQuery", "jsutils.file", "jsutils.tmpl"]
+}).as(function(i18n, jQuery, fileUtil, tempUtil) {
+	var STRINGS = {};
+	return {
+		_instance_ : function() {
+			this.reversemap = {};
+		},
+		add : function(file) {
+			if (is.String(file)) {
+				return fileUtil.getJSON(file).done(function(resp) {
+					for (var key in resp.map) {
+						STRINGS[key] = resp.map[key];
+					}
+				});
+			} else {
+				return jQuery.Deferred(function(dff) {
+					for (var key in file.map) {
+						STRINGS[key] = file.map[key];
+					}
+					dff.resolve(file);
+				}).promise();
+			}
+		},
+		get : function(key) {
+			if (is.String(key)) {
+				var keys = key.split(":");
+				var str = STRINGS[keys[0]] || "";
+				for (var i = 1; i < keys.length; i++) {
+					str = str.replace("$" + i, keys[i]);
+				}
+				str = str ? str : key;
+				if (this.reversemap) {
+					this.reversemap[str] = key;
+				}
+				return str;
+			}
+			return key;
+		},
+		key : function(str) {
+			if (is.String(str) && this.reversemap && this.reversemap[str]) {
+				return this.reversemap[str];
+			}
+			return str;
+		},
+		_ready_ : function() {
+			var i18n = this;
+			if (tempUtil) {
+				tempUtil.formatter("i18n", function(key) {
+					return i18n.get(key);
+				});
+			}
+		}
+	};
 });
